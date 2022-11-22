2cd81b75bc114d5f3d3422811d6e8343db6efe77
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2022-11-22 20:52:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿public class Kata
+{
+    public static int Divisors(int n)
+    {
+        // todo
+        var numberOfDivisors = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                numberOfDivisors++;
+            }
+        }
+        return numberOfDivisors;
+    }
+}
